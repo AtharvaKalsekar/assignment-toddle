989e8b5a7ac62cdbf1669b62927f759a8810940b
AtharvaKalsekar <kalsekar.atharva7@gmail.com>
2022-11-27 14:43:46 +0530

    modifies draggable item

--- src/modules/RowList/RowList.tsx
+++ src/modules/RowList/RowList.tsx
@@ -1,8 +1,17 @@
 import { DraggableItem } from '@components';
+import { useMemo, useRef } from 'react';
 
 import { Row } from './Row';
 
 export const RowList = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [gridHeight, gridWidth] = useMemo(() => {
+    const height = ref.current?.offsetHeight || 0;
+    const width = ref.current?.offsetWidth || 0;
+    return [height, width];
+  }, []);
+
   return (
     <div>
       {Array.from({ length: 5 }).map((_, index) => {
@@ -11,8 +20,11 @@ export const RowList = () => {
             handle={`#drag_handle_${index}`}
             axis="y"
             bounds="parent"
+            grid={[gridWidth, gridHeight]}
+            key={index}
+            position={{ x: 0, y: gridHeight }}
           >
-            <div>
+            <div ref={ref}>
               <Row id={index} />
             </div>
           </DraggableItem>
